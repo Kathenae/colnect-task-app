2d62fb78bccce8de6fa1a2d7677356bbf7681cd5
Ronald <RonaldWilsonMachado@gmail.com>
2024-01-17 15:33:41 +0200

    Refactor CountAPIController and Request logic
    Streamlined CountAPIController to reduce complexity and improve
    readability.
    Updated Request model to include a constructor allowing direct
    instantiation with all necessary attributes, which facilitates cleaner
    and more concise object creation within the controller.
    
    This commit also introduces statistics calculations directly within the
    Request model, encapsulating related logic and enhancing
    maintainability. The combination of these changes results in a cleaner
    API, promotes better separation of concerns, and aligns with best
    practices for object-oriented design.

--- src/Controllers/CountAPIController.php
+++ src/Controllers/CountAPIController.php
@@ -8,11 +8,7 @@ use Elemizer\App\Components\Database;
 use Elemizer\App\Components\ErrorBag;
 use Elemizer\App\Components\HtmlInspector;
 use Elemizer\App\Components\HttpClient\HttpClient;
-use Elemizer\App\Components\HttpClient\HttpClientResponse;
-use Elemizer\App\Models\Domain;
-use Elemizer\App\Models\Element;
-use Elemizer\App\Models\Request;
-use Elemizer\App\Models\Url;
+use Elemizer\App\Models;
 
 class CountAPIController
 {
@@ -34,7 +30,10 @@ class CountAPIController
       // Fetch URL and handle errors.
       $httpClient = new HttpClient($targetUrl);
       $response = $httpClient->request($targetUrl);
-      $this->handleErrors($response);
+
+      if ($response->statusIsOkay() == false) {
+         APIResponse::emitErrorMessage('There was a problem analysing the page.');
+      }
 
       // Parse response data
       $elementCount = HtmlInspector::countElement($targetElement, $response->getBody());
@@ -42,36 +41,20 @@ class CountAPIController
       $duration = $response->getTotalDurationMs();
       $time = new DateTime();
 
-      $this->saveRequestDetails($targetElement, $elementCount, $domainName, $targetUrl, $time, $duration);
-      $stats = $this->getStats($domainName, $targetElement, $targetElement);
+      // Create request info with obtained info
+      $request = Models\Request::create(
+         elementName: $targetElement,
+         elementCount: $elementCount,
+         domainName: $domainName,
+         urlName: $targetUrl,
+         time: $time,
+         duration: $duration
+      );
 
-      APIResponse::emitSuccessData([
-         'domainName' => $domainName,
-         'url' => $targetUrl,
-         'element' => $targetElement,
-         'count' => $elementCount,
-         'fetchDurationMs' => $duration,
-         'fetchedAt' => $time,
-         'stats' => $stats,
-      ]);
-   }
+      Database::manager()->persist($request);
+      Database::manager()->flush();
 
-   /**
-    * Handles errors in the HTTP response.
-    *
-    * @param HttpClientResponse $response The HTTP response.
-    * @return void
-    */
-   private function handleErrors(HttpClientResponse $response)
-   {
-      if ($response->statusIsOkay() == false) {
-         if ($response->statusIsRedirect()) {
-            // TODO: Maybe for 301, 302 and 307 redirects we should fetch the page we're redirected to instead
-            APIResponse::emitErrorMessage('Remote server responded with an unhandled redirect response');
-         } else {
-            APIResponse::emitErrorMessage('Remote server responded with an unknown error response');
-         }
-      }
+      APIResponse::emitSuccessData($request->results());
    }
 
    /**
@@ -103,101 +86,6 @@ class CountAPIController
       return [
          self::FIELD_URL => $targetUrl,
          self::FIELD_ELEMENT => $targetElement,
-      ];
-   }
-
-   /**
-    * Saves the details of the request to the database.
-    *
-    * @param string   $targetElement The target HTML element.
-    * @param int      $elementCount  The count of the target element in the web page.
-    * @param string   $domainName    The domain name of the web page.
-    * @param string   $targetUrl     The target URL.
-    * @param DateTime $dateTime      The date and time of the request.
-    * @param float    $duration      The duration of the request.
-    * @return Request The saved Request object.
-    */
-   private function saveRequestDetails(string $targetElement, int $elementCount, string $domainName, string $targetUrl, DateTime $dateTime, float $duration)
-   {
-      // Check if the Domain already exists
-      $domain = Database::manager()->getRepository(Domain::class)->findOneBy(['name' => $domainName]);
-
-      if (!$domain) {
-         // Create a new Domain if it doesn't exist
-         $domain = new Domain();
-         $domain->setName($domainName);
-         Database::manager()->persist($domain);
-      }
-
-      // Check if the Url already exists
-      $url = Database::manager()->getRepository(Url::class)->findOneBy(['name' => $targetUrl]);
-
-      if (!$url) {
-         // Create a new Url if it doesn't exist
-         $url = new Url();
-         $url->setName($targetUrl);
-         Database::manager()->persist($url);
-      }
-
-      // Check if the Element already exists
-      $element = Database::manager()->getRepository(Element::class)->findOneBy(['name' => $targetElement]);
-
-      if (!$element) {
-         // Create a new Element if it doesn't exist
-         $element = new Element();
-         $element->setName($targetElement);
-         Database::manager()->persist($element);
-      }
-
-      // Create a new Request
-      $request = new Request();
-      $request->setDuration($duration);
-      $request->setElementCount($elementCount);
-      $request->setTime($dateTime);
-      $request->setDomain($domain);
-      $request->setUrl($url);
-      $request->setElement($element);
-
-      Database::manager()->persist($request);
-      Database::manager()->flush();
-
-      return $request;
-   }
-
-   /**
-    * Retrieves the statistics for the given domain and target element.
-    *
-    * @param string $domainName    The domain name.
-    * @param string $targetElement The target HTML element.
-    * @return array An associative array containing the statistics.
-    */
-   private function getStats(string $domainName, string $elementName)
-   {
-      $entityManager = Database::manager();
-
-      $query = $entityManager->createQuery('SELECT COUNT(DISTINCT(r.url)) FROM Elemizer\App\Models\Request r JOIN r.domain d where d.name = :domainName');
-      $query->setParameter('domainName', $domainName);
-      $urlCount = $query->getSingleScalarResult();
-
-      $query = $entityManager->createQuery('SELECT AVG(r.duration) FROM Elemizer\App\Models\Request r JOIN r.domain d WHERE d.name = :domainName AND r.time >= :date');
-      $query->setParameter('domainName', $domainName);
-      $query->setParameter('date', new DateTime('-24 hours'));
-      $averageFetchTime = $query->getSingleScalarResult();
-
-      $query = $entityManager->createQuery('SELECT SUM(r.elementCount) FROM Elemizer\App\Models\Request r JOIN r.domain d JOIN r.element e WHERE d.name = :domainName AND e.name = :elementName');
-      $query->setParameter('domainName', $domainName);
-      $query->setParameter('elementName', $elementName);
-      $elementCountDomain = $query->getSingleScalarResult();
-
-      $query = $entityManager->createQuery('SELECT SUM(r.elementCount) FROM Elemizer\App\Models\Request r JOIN r.element e WHERE e.name = :elementName');
-      $query->setParameter('elementName', $elementName);
-      $elementCountAll = $query->getSingleScalarResult();
-
-      return [
-         'urlCount' => $urlCount,
-         'averageFetchTime' => $averageFetchTime,
-         'elementCountDomain' => $elementCountDomain,
-         'elementCountAll' => $elementCountAll
       ];
    }
 }
--- src/models/Request.php
+++ src/models/Request.php
@@ -5,6 +5,7 @@ namespace Elemizer\App\Models;
 use DateTime;
 use Doctrine\ORM\Mapping as ORM;
 use Doctrine\ORM\Mapping\ManyToOne;
+use Elemizer\App\Components\Database;
 
 #[ORM\Entity]
 class Request
@@ -14,81 +15,149 @@ class Request
    #[ORM\GeneratedValue]
    private int|null $id;
 
-   #[ORM\Column(type: 'float')]
-   private float $duration;
+   public function __construct(
+      #[ORM\Column(type: 'float')]
+      private float $duration,
 
-   #[ORM\Column(type: 'integer')]
-   private int $elementCount;
+      #[ORM\Column(type: 'integer')]
+      private int $elementCount,
 
-   #[ORM\Column(type: 'datetime')]
-   private DateTime $time;
+      #[ORM\Column(type: 'datetime')]
+      private DateTime $time,
 
-   #[ManyToOne(targetEntity: Domain::class)]
-   private Domain $domain;
+      #[ManyToOne(targetEntity: Domain::class)]
+      private Domain $domain,
 
-   #[ManyToOne(targetEntity: Url::class)]
-   private Url $url;
+      #[ManyToOne(targetEntity: Url::class)]
+      private Url $url,
 
-   #[ManyToOne(targetEntity: Element::class)]
-   private Element $element;
-
-   public function setDomain(Domain $domain)
-   {
-      $this->domain = $domain;
+      #[ManyToOne(targetEntity: Element::class)]
+      private Element $element
+   ) {
    }
 
-   public function getDomain(): Domain
+   public function results()
    {
-      return $this->domain;
+      $urlCount = $this->getTotalUrlAppearances();
+      $averageFetchTime = $this->getAvgResponseTime(new DateTime('-24 hours'));
+      $elementCountDomain = $this->countElementsForDomain();
+      $elementCountAll = $this->countElementsForAllRequest();
+
+      return [
+         'domain' => $this->domain->getName(),
+         'url' => $this->url->getName(),
+         'elementName' => $this->element->getName(),
+         'fetchedAt' => $this->time,
+         'urlCount' => $urlCount,
+         'fetchTime' => $this->duration,
+         'averageFetchTime' => $averageFetchTime,
+         'elementCountDomain' => $elementCountDomain,
+         'elementCountAll' => $elementCountAll
+      ];
    }
 
-   public function setUrl(Url $url)
+   public function getTotalUrlAppearances()
    {
-      $this->url = $url;
+      $entityManager = Database::manager();
+
+      $query = $entityManager->createQuery(<<<EOD
+      SELECT 
+         COUNT(DISTINCT(r.url)) 
+      FROM 
+         Elemizer\App\Models\Request r 
+      JOIN 
+         r.domain d where d = :domain
+      EOD);
+
+      $query->setParameter('domain', $this->domain);
+      $count = $query->getSingleScalarResult();
+      return $count;
    }
 
-   public function getUrl(): Url
+   public function getAvgResponseTime(DateTime $timespan)
    {
-      return $this->url;
+      $query = Database::manager()->createQuery(<<<EOD
+      SELECT
+          AVG(r.duration) 
+      FROM Elemizer\App\Models\Request r 
+      JOIN 
+         r.domain d WHERE d = :domain AND r.time >= :timespan         
+      EOD);
+      $query->setParameter('domain', $this->domain);
+      $query->setParameter('timespan', $timespan);
+      $avg = $query->getSingleScalarResult();
+      return $avg;
    }
 
-   public function setElement(Element $element)
+   public function countElementsForDomain()
    {
-      $this->element = $element;
+      $query = Database::manager()->createQuery(<<<EOD
+      SELECT 
+         SUM(r.elementCount) 
+      FROM Elemizer\App\Models\Request r 
+      JOIN 
+         r.domain d 
+      JOIN 
+         r.element e 
+      WHERE 
+         d = :domain AND e = :element
+      EOD);
+      $query->setParameter('domain', $this->domain);
+      $query->setParameter('element', $this->element);
+      $count = $query->getSingleScalarResult();
+      return $count;
    }
 
-   public function getElement(): Element
+   public function countElementsForAllRequest()
    {
-      return $this->element;
+      $query = Database::manager()->createQuery(<<<EOD
+      SELECT 
+         SUM(r.elementCount) 
+      FROM 
+         Elemizer\App\Models\Request r 
+      JOIN 
+         r.element e 
+      WHERE e = :element
+      EOD);
+      $query->setParameter('element', $this->element);
+      $count = $query->getSingleScalarResult();
+      return $count;
    }
 
-   public function setTime(DateTime $time)
+   public static function create(string $elementName, int $elementCount, string $domainName, string $urlName, DateTime $time, float $duration)
    {
-      $this->time = $time;
-   }
+      // find domain or create new
+      $domain = Database::manager()->getRepository(Domain::class)->findOneBy(['name' => $domainName]);
+      if (!$domain) {
+         $domain = new Domain();
+         $domain->setName($domainName);
+         Database::manager()->persist($domain);
+      }
 
-   public function getTime(): DateTime
-   {
-      return $this->time;
-   }
+      $url = Database::manager()->getRepository(Url::class)->findOneBy(['name' => $urlName]);
+      if (!$url) {
+         $url = new Url();
+         $url->setName($urlName);
+         Database::manager()->persist($url);
+      }
 
-   public function setDuration(float $duration)
-   {
-      $this->duration = $duration;
-   }
+      $element = Database::manager()->getRepository(Element::class)->findOneBy(['name' => $elementName]);
+      if (!$element) {
+         $element = new Element();
+         $element->setName($elementName);
+         Database::manager()->persist($element);
+      }
 
-   public function getDuration(): float
-   {
-      return $this->duration;
-   }
+      // Create a new Request
+      $request = new Request(
+         domain: $domain,
+         url: $url,
+         element: $element,
+         elementCount: $elementCount,
+         duration: $duration,
+         time: $time,
+      );
 
-   public function setElementCount(int $elementCount)
-   {
-      $this->elementCount = $elementCount;
-   }
-
-   public function getElementCount(): float
-   {
-      return $this->elementCount;
+      return $request;
    }
 }
