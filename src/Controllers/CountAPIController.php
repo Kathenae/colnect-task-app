68f4fa328c5070c133177a0703d9e1961b9be0cf
Ronald <RonaldWilsonMachado@gmail.com>
2024-01-17 22:41:06 +0200

    Enhance error messaging for URL fetching

--- src/Controllers/CountAPIController.php
+++ src/Controllers/CountAPIController.php
@@ -36,7 +36,7 @@ class CountAPIController
             $response = $httpClient->request($targetUrl);
 
             if ($response->statusIsOkay() == false) {
-               APIResponse::emitErrorMessage();
+               APIResponse::emitErrorMessage("Unable to fetch URL. The server might have some restrictions or an error occurred internally");
             }
 
             $request = Models\Request::create(
